70e05a8cdcc6360496e33f692b6426da18a4dede
Andrea Benato <benato.andrea98@gmail.com>
2020-03-30 15:10:25 +0200

    add variables section

---
+++ variables.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser:true,
+        node:true
+    },
+    rules: {
+        'init-declarations': 'off', // are you kidding me
+        'no-delete-var': 'error',
+        'no-label-var': 'error',
+        'no-restricted-globals': ['error', 'event', 'fdescribe'],
+        'no-shadow': 'error',
+        'no-shadow-restricted-names': 'error',
+        'no-undef': 'error',
+        'no-undef-init': 'error',
+        'no-undefined': 'off',
+        'no-unused-vars': [
+            'error',
+            {
+              argsIgnorePattern: '^_',
+              varsIgnorePattern: '^ignored',
+              args: 'after-used',
+              ignoreRestSiblings: true,
+            },
+          ],
+        'no-use-before-define' : 'off' // sometimes could be really annoying
+    }
+}
