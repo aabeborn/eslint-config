ee3391eca3fd4b4f5356c27f04ef552e2b6f723a
Andrea Benato <benato.andrea98@gmail.com>
2020-03-30 22:06:02 +0200

    add react configuration (#5)

---
+++ react.js
@@ -0,0 +1,103 @@
+module.exports = {
+	extends: ['prettier/react'],
+	env: {
+		browser: true,
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ['react', 'react-hooks'],
+	settings: {
+		react: {
+			version: 'default',
+		},
+	},
+	rules: {
+		'react/boolean-prop-naming': 'off', // don't uderstand
+		'react/button-has-type': 'warn',
+		'react/default-props-match-prop-types': 'warn',
+		'react/destructuring-assignment': ['off', 'always'], // great result if someone does it ahahah
+		'react/display-name': ['error', { ignoreTranspilerName: false }],
+		'react/forbid-component-props': 'off', // have no props to add here
+		'react/forbid-dom-props': 'off', // same here
+		'react/forbid-elements': 'off',
+		'react/forbid-foreign-prop-types': 'error',
+		'react/forbid-prop-types': 'off',
+		'react/function-component-definition': 'off',
+		'react/no-access-state-in-setstate': 'error',
+		'react/no-adjacent-inline-elements': 'warn',
+		'react/no-array-index-key': 'off',
+		'react/no-children-prop': 'off',
+		'react/no-danger': 'off',
+		'react/no-danger-with-children': 'error',
+		'react/no-deprecated': 'error',
+		'react/no-did-mount-set-state': 'error',
+		'react/no-did-update-set-state': 'error',
+		'react/no-direct-mutation-state': 'error',
+		'react/no-find-dom-node': 'error',
+		'react/no-is-mounted': 'error',
+		'react/no-multi-comp': 'off',
+		'react/no-redundant-should-component-update': 'error',
+		'react/no-render-return-value': 'error',
+		'react/no-set-state': 'off', // not using flux or state manager
+		'react/no-string-refs': 'error',
+		'react/no-this-in-sfc': 'error',
+		'react/no-typos': 'error',
+		'react/no-unescaped-entities': 'warn',
+		'react/no-unknown-property': 'error',
+		'react/no-unsafe': 'warn',
+		'react/no-unused-prop-types': 'error',
+		'react/no-unused-state': 'error',
+		'react/no-will-update-set-state': 'error',
+		'react/prefer-es6-class': 'off',
+		'react/prefer-read-only-props': 'off',
+		'react/prefer-stateless-function': 'off',
+		'react/prop-types': 'error',
+		'react/react-in-jsx-scope': 'error',
+		'react/require-default-props': 'off',
+		'react/require-optimization': 'off',
+		'react/require-render-return': 'error',
+		'react/self-closing-comp': 'error',
+		'react/sort-comp': 'off',
+		'react/sort-prop-types': 'off',
+		'react/state-in-constructor': 'off',
+		'react/static-property-placement': 'off',
+		'react/style-prop-object': 'error',
+		'react/void-dom-elements-no-children': 'error',
+
+		'react-hooks/exhaustive-deps': 'warn',
+		'react-hooks/rules-of-hooks': 'error',
+
+		// jsx
+		'react/jsx-boolean-value': 'off',
+		'react/jsx-curly-brace-presence': [
+			'warn',
+			{ props: 'never', children: 'ignore' },
+		],
+		'react/jsx-filename-extension': [
+			'error',
+			{ extensions: ['.js', '.ts'] },
+		],
+		'react/jsx-fragments': 'off',
+		'react/jsx-handler-names': 'off',
+		'react/jsx-key': 'error', // I'm not flexible here
+		'react/jsx-max-depth': 'off',
+		'react/jsx-no-bind': 'off',
+		'react/jsx-no-comment-textnodes': 'error',
+		'react/jsx-no-duplicate-props': 'error',
+		'react/jsx-no-literals': 'off',
+		'react/jsx-no-script-url': 'error',
+		'react/jsx-no-target-blank': 'error',
+		'react/jsx-no-undef': 'error',
+		'react/jsx-no-useless-fragment': 'warn',
+		'react/jsx-pascal-case': 'error',
+		'react/jsx-props-no-spreading': 'off',
+		'react/jsx-sort-default-props': 'off',
+		'react/jsx-sort-props': 'off',
+		'react/jsx-space-before-closing': 'off',
+		'react/jsx-uses-react': 'error',
+		'react/jsx-uses-vars': 'error',
+	},
+}
