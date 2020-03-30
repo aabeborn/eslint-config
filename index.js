ddf49270623cfb94287e4489a27df9c12e15b66b
Andrea Benato <benato.andrea98@gmail.com>
2020-03-30 15:30:32 +0200

    Revert "add the common eslint configuration"

--- index.js
+++ index.js
@@ -4,7 +4,4 @@ Main configuration
 ==========================
 */
 // it contains all the common rules that works for all kind of projects
-module.exports = {
-	extends: ['./stylistic.js', './possibleErrors.js'],
-	rules: {},
-}
+module.exports = {}
