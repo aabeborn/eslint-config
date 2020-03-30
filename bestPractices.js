5e96d23c36d3519e0eb89620f7b9366be24d54d9
Andrea Benato <benato.andrea98@gmail.com>
2020-03-30 15:26:35 +0200

    added files to index, small fix and formatting

--- bestPractices.js
+++ bestPractices.js
@@ -1,6 +1,6 @@
 module.exports = {
 	env: {
-		broswser: true,
+		browser: true,
 		node: true,
 	},
 	rules: {
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ Main configuration
 */
 // it contains all the common rules that works for all kind of projects
 module.exports = {
-	extends: ['./stylistic.js', './possibleErrors.js'],
+	extends: [
+		'./stylistic.js',
+		'./possibleErrors.js',
+		'./bestPractices.js',
+		'./variables.js',
+	],
 	rules: {},
 }
